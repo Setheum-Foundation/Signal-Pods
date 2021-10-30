d9c01a50f281b1ab6713fe58ab37d6c6bd0b4301
Michelle Linington <michelle@signal.org>
2021-10-29 19:36:37 -0700

    Update Pods

--- Target Support Files/Pods-SignalNSE/Pods-SignalNSE-umbrella.h
+++ Target Support Files/Pods-SignalNSE/Pods-SignalNSE-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_NotificationServiceExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_NotificationServiceExtensionVersionString[];
+FOUNDATION_EXPORT double Pods_SignalNSEVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SignalNSEVersionString[];
 
